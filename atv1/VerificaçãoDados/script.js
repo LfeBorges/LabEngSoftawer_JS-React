21c46a095e9ac6a529592fff05943402eced4038
Luiz Felipe <luiz.bore2004@gmail.com>
2025-02-23 21:35:14 -0300

    atv4 semana1
    correção da atividade de Verificação de dado

--- atv1/VerificaçãoDados/script.js
+++ atv1/VerificaçãoDados/script.js
@@ -1,7 +1,7 @@
 let dado = prompt("Digite um dado:");
 
 if (dado !== null) { 
-    let verificarTipo = confirm("Você deseja verificar o tipo do dado informado?");
+    const verificarTipo = confirm("Você deseja verificar o tipo do dado informado?");
     
     if (verificarTipo) {
        
@@ -13,8 +13,8 @@ if (dado !== null) {
             dado = (dado.toLowerCase() === "true");
         }
 
-        document.body.innerHTML = `<h1>O tipo do dado informado é: ${typeof dado}</h1>`;
+         alert("O tipo do dado informado é: " + typeof dado);
     } else {
-        document.body.innerHTML = "<h1>Dado não confirmado</h1>";
+        alert("Dado não confirmado");
     }
 }
