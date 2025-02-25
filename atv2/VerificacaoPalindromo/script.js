be6b72991043e9106093ab6915d92fc36fc4dbac
LfeBorges <luiz.bore2004@gmail.com>
2025-02-24 21:03:27 -0300

    Palindromo correção

--- atv2/VerificacaoPalindromo/script.js
+++ atv2/VerificacaoPalindromo/script.js
@@ -1,16 +1,11 @@
-function verificarPalindromo() {
-    var texto = prompt("Digite uma palavra ou frase:");  
-    var textoInvertido = "";  
-
-    for (var i = texto.length - 1; i >= 0; i--) {
-        textoInvertido += texto[i];
-    }
-
-    if (texto === textoInvertido) {
-        alert("A palavra ou frase é um palíndromo!");
-    } else {
-        alert("A palavra ou frase não é um palíndromo.");
-    }
+function verificarPalindromo(frase) {
+    frase = frase.replace(/\s/g, '').toLowerCase();
+    return frase === frase.split('').reverse().join('');
 }
 
-verificarPalindromo();
+const entrada = prompt("Digite uma palavra ou frase:");
+if (verificarPalindromo(entrada)) {
+    alert(`'${entrada}' é um palíndromo!`);
+} else {
+    alert(`'${entrada}' não é um palíndromo.`);
+}
